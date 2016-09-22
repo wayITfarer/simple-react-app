f7309cbb88d2d07f051aebd8a743062986a0fd8c
sergey <s.minlevskiy@gmail.com>
2016-09-22 18:22:10 +0300

    React start app

--- webpack.config.js
+++ webpack.config.js
@@ -17,10 +17,10 @@ module.exports = {
     }]
   },
   resolve: {
-    extentions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx']
   },
   devServer: {
-    historyAppFallback: true,
-    contentBase: '/'
+    historyApiFallback: true,
+    contentBase: './'
   }
 }
