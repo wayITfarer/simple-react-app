e6dc9a2c0e8194dfcc6b280bdd989d777b58e55a
sergey <s.minlevskiy@gmail.com>
2016-09-22 11:27:18 +0300

    Set up the project

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-0']
+      }
+    }]
+  },
+  resolve: {
+    extentions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyAppFallback: true,
+    contentBase: '/'
+  }
+}
